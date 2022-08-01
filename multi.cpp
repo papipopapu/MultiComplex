577d9522f16db27cedc1b7e9f4c1f850e36cce67
Joel Martinez <yoyomartinez314@gmail.com>
2022-08-01 02:23:15 +0200

    sdghdfhdfh

--- multi.cpp
+++ multi.cpp
@@ -5,71 +5,83 @@
 #include<algorithm>
 
 using Complex = std::complex<double>;
-#define MAX(N1, N2) compare<(N1>N2), N1, N2>::ans
-
-
-template <bool, unsigned N1, unsigned N2>
-struct compare {
-    static const unsigned ans = N1;
-};
-template <unsigned N1, unsigned N2>
-struct compare<false, N1, N2> {
-    static const unsigned ans = N2;
-};
-
-
-
-
-
 
 template<unsigned N>
-class MultiComplex {
-    public:
-    uint n;
-    MultiComplex<N-1> z1, z2;    
-    MultiComplex<N>() {n = N;}
-    void set_z1(Complex z) {
-        z1.set_z1(z);
-    }
-    void set_z2(Complex z) {
-        z2.set_z1(z);
-    }
+class MultiComplex;
 
+template<unsigned N> 
+struct compare {
+    typedef MultiComplex<N> type;
+};
+
+template<>
+struct compare<0U> {
+    typedef Complex type;
+};
+
+#define TYPE(N) typename compare<N>::type
+
+
+template<unsigned N> // bicomplex as N = 1
+class MultiComplex { // only allow operations between same order MultiComplex
+private:
+    TYPE(N-1) z1, z2;  
+    uint n;
+public:
+    MultiComplex<N>() : n(N) {} 
+    MultiComplex<N>(TYPE(N-1) z1_, TYPE(N-1) z2_) : n(N), z1(z1_), z2(z2_) {}   
+    MultiComplex<N>& operator=(MultiComplex<N> w) {
+        std::swap(w);
+        return *this;
+    }
+    void setRe(Complex z) { 
+        auto next = this;
+        for (uint i = 1; i < N; i++) {
+            auto tmp = next;
+            auto next = tmp->Re();
+        }
+        next->Re() = z;
+    }
+    void setIm(Complex z) { 
+        auto next = this;
+        for (uint i = 1; i < N; i++) {
+            auto tmp = next;
+            if (i == 1) {
+                auto next = tmp->Re();
+            } else {
+            auto next = tmp->Re();
+            }
+        }
+        if (N == 1) {
+            next->Im() = z;
+        } else {
+            next->Re() = z;
+        }
+    }
     
-};
-template <>
-class MultiComplex<2> {
-    public:
-    uint n;
-    Complex z1, z2;
-    MultiComplex<2>() {n = 2;}
-    void set_z1(Complex z) {
-        z1 = z;
-    }
-    void set_z2(Complex z) {
-        z2 = z;
-    }
-   
+    TYPE(N-1)&Re() { return z1; } 
+    TYPE(N-1)&Im() { return z2; }
+    TYPE(N-1) Re() const { return z1; }
+    TYPE(N-1) Im() const { return z2; }
 };
 
-template <unsigned N1, unsigned N2>
-MultiComplex<MAX(N1, N2)> test(MultiComplex<N1> z1, MultiComplex<N2> z2) {
-    MultiComplex<MAX(N1, N2)> z;
-    return z1;
-}
 
 int main() {
     
 
-    MultiComplex<3> x;
-    MultiComplex<2> y;
-    auto z = test(x, y);
-    std::cout << z.n << std::endl; 
+    
+    MultiComplex<1> x;
+    x.setRe(Complex(1, 2));
+    x.setIm(Complex(0, 1));
 
-    MultiComplex<4> x2;
-    MultiComplex<3> y2;
-    auto w = test(x2, y2);
-    std::cout << w.n << std::endl; 
+    MultiComplex<1> y;
+    y.setRe(Complex(2, 3));
+    y.setIm(Complex(3, 2));
+
+
+    std::cout << x.Re() << std::endl;
+
+
 
     
     return 0;
