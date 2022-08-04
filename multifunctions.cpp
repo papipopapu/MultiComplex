84ea22a1745dc77cd8054c50d7dba94bf0e36997
Joel Martinez <yoyomartinez314@gmail.com>
2022-08-04 14:07:44 +0200

    kgrnk

--- multifunctions.cpp
+++ multifunctions.cpp
@@ -4,6 +4,40 @@ namespace MComplex {
     static constexpr int HALLEY_ITER = 5;
 
 
+    template<class T>
+    T sin(const T& val) { return std::sin(val); }
+    template<class T>
+    T cos(const T& val) { return std::cos(val); }
+    template<class T>
+    T tan(const T& val) { return std::tan(val); }
+    template<class T>
+    T sinh(const T& val) { return std::sinh(val); }
+    template<class T>
+    T cosh(const T& val) { return std::cosh(val); }
+    template<class T>
+    T tanh(const T& val) { return std::tanh(val); }
+    template<class T>
+    T asin(const T& val) { return std::asin(val); }
+    template<class T>
+    T acos(const T& val) { return std::acos(val); }
+    template<class T>
+    T atan(const T& val) { return std::atan(val); }
+    template<class T>
+    T asinh(const T& val) { return std::asinh(val); }
+    template<class T>
+    T acosh(const T& val) { return std::acosh(val); }
+    template<class T>
+    T atanh(const T& val) { return std::atanh(val); }
+
+    
+    template<class T>
+    T log(const T& val) { return std::log(val); }
+    template<class T>
+    T exp(const T& val) { return std::exp(val); }
+
+
+
+
     template<unsigned N, class T>
     MultiComplex<N, T> tan(const MultiComplex<N, T>& z);
     template<unsigned N, class T>
@@ -35,6 +69,10 @@ namespace MComplex {
     template<unsigned N, class T>
     MultiComplex<N, T> atan(const MultiComplex<N, T>& z);
     
+
+
+
+
     template<unsigned N, class T>
     MultiComplex<N, T> tan(const MultiComplex<N, T>& z) {
         MultiComplex<N, T> ret = sin(z)/cos(z);      
@@ -63,10 +101,8 @@ namespace MComplex {
     }
     template<unsigned N, class T>
     MultiComplex<N, T> exp(const MultiComplex<N, T>& z) {
-        std::cout << "//// exp ("<<N<< ")  args{"<< z <<"}" << std::endl;  
-        MultiComplex<N-1, T> r  = exp(z.real());
+        auto r  = exp(z.real());
         MultiComplex<N, T> ret{r * cos(z.imag()), r * sin(z.imag())};  
-        std::cout << "exp(" << N << "): " << ret << std::endl;  
         return ret;
     }
     template<unsigned N, class T>
@@ -86,6 +122,15 @@ namespace MComplex {
         ret = z;
         for (int i = 1; i < n; i++) {
             ret *= z;
+        }
+        return ret;
+    }
+    template<unsigned N, class T>
+    MultiComplex<N, T> atan(const MultiComplex<N, T>& z) {
+        const int terms = 5;
+        MultiComplex<N, T> ret;
+        for (int n = 0; n < EXPANSION_TERMS; n++) {
+            ret += ((-1)^n) * pow(z, 2*n+1) / (2*n+1);
         }
         return ret;
     }
